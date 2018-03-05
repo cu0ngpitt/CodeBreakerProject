9f09fa222d04ddff1e4e675fd4a6e1aa3c77968f
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-03-05 15:38:52 -0600

    k

--- src/assets/main.js
+++ src/assets/main.js
@@ -20,3 +20,11 @@ function setHiddenFields() {
 function setMessage(x) {
   document.getElementById("message").innerHTML = x;
 }
+function validateInput(y) {
+  if (y.length < 4) {
+    return true;
+  } else {
+    setMessage("Guesses must be exactly 4 characters long.");
+    return false;
+  }
+}
