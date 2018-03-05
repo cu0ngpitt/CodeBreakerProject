9aaedc670bfdc4258f01b081e1ba5895627bad18
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-03-05 15:26:00 -0600

    a

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,13 +3,17 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
+    if (answer.value === "" || attempt.value ==="") {
+      setHiddenFields();
+    }
     //add functionality to guess function here
 }
 
 //implement new functions here
 function setHiddenFields() {
-  answer.value = Math.floor(Math.random() * 10000);
+  answer.value = Math.floor(Math.random() * 10000).toString();
   while (answer.value.length <= 4) {
-    return answer.value.toString();
+    answer.value = "0" + answer.value;
   }
+  attempt.value = 0
 }
